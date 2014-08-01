d43e387d56af8fc3db0bb36323a788096437b7e9
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-08-01 20:53:00 +0000

    Added functionality for setting font styles
    -Added enum class that holds all font styles
    -Added set/get/remove functions for setting or getting font styles.
    These all work on the mask holding the font style using bitwise
    operators
    -Also added a function for setting the mask directly, the user then has
    to set the flags himself using the bitwsie operators
    -Added function for applying font style that only updates the font if
    the mask has changed

--- Font.h
+++ Font.h
@@ -1,5 +1,13 @@
 #pragma once
 
+enum class FontStyle
+{
+	Normal,
+	Bold,
+	Italic,
+	Underline,
+	Strikethrough
+};
 class Font
 {
 	public:
@@ -25,11 +33,119 @@ class Font
 				std::cout << "Failed to load font";
 				return false;
 			}
-		}
 
-	
+			return true;
+		}
+		void AddFontStyle( FontStyle style )
+		{
+			switch ( style )
+			{
+				case FontStyle::Normal:
+					return;
+				case FontStyle::Bold:
+					mask |= TTF_STYLE_BOLD;
+					break;
+				case FontStyle::Italic:
+					mask |= TTF_STYLE_ITALIC;
+					break;
+				case FontStyle::Underline:
+					mask |= TTF_STYLE_UNDERLINE;
+					break;
+				case FontStyle::Strikethrough:
+					mask |= TTF_STYLE_STRIKETHROUGH;
+					break;
+				default :
+					break;
+			}
+			TTF_SetFontStyle( font, mask );
+
+			ApplyFontStyle();
+		}
+		void RemFontStyle( FontStyle style )
+		{
+			switch ( style )
+			{
+				case FontStyle::Normal:
+					break;
+				case FontStyle::Bold:
+					mask &= ~TTF_STYLE_BOLD;
+					break;
+				case FontStyle::Italic:
+					mask &= ~TTF_STYLE_ITALIC;
+					break;
+				case FontStyle::Underline:
+					mask &= ~TTF_STYLE_UNDERLINE;
+					break;
+				case FontStyle::Strikethrough:
+					mask &= ~TTF_STYLE_STRIKETHROUGH;
+					break;
+				default :
+					break;
+			}
+
+			ApplyFontStyle();
+		}
+		void SetFontStyle( FontStyle style )
+		{
+			switch ( style )
+			{
+				case FontStyle::Normal:
+					mask = TTF_STYLE_NORMAL;
+					break;
+				case FontStyle::Bold:
+					mask = TTF_STYLE_BOLD;
+					break;
+				case FontStyle::Italic:
+					mask = TTF_STYLE_ITALIC;
+					break;
+				case FontStyle::Underline:
+					mask = TTF_STYLE_UNDERLINE;
+					break;
+				case FontStyle::Strikethrough:
+					mask = TTF_STYLE_STRIKETHROUGH;
+					break;
+				default :
+					break;
+			}
+
+			ApplyFontStyle();
+		}
+		bool ChkFontStyle( FontStyle style )
+		{
+			switch ( style )
+			{
+				case FontStyle::Normal:
+					return ( mask & TTF_STYLE_NORMAL );
+				case FontStyle::Bold:
+					return ( mask & TTF_STYLE_BOLD );
+				case FontStyle::Italic:
+					return ( mask & TTF_STYLE_ITALIC );
+				case FontStyle::Underline:
+					return ( mask & TTF_STYLE_UNDERLINE );
+				case FontStyle::Strikethrough:
+					return ( mask & TTF_STYLE_STRIKETHROUGH );
+				default :
+					return false;
+			}
+
+			ApplyFontStyle();
+		}
+		void SetFontStyleMask( int32_t newMask )
+		{
+			mask = newMask;
+		}
+		void ApplyFontStyle()
+		{
+			if ( prevMask == mask )
+				return;
+			
+			TTF_SetFontStyle( font, mask );
+
+			prevMask = mask;
+		}
 	private:
 		TTF_Font* font;
 
+		int32_t prevMask;
 		int32_t mask;
 };
