ec8d23e4cb1bf808875aa3a87511d74cae75c7d5
Ole Vegard Mythe Moland <oleveg@gmail.com>
2014-08-01 20:53:00 +0000

    Improved Init()

--- Font.h
+++ Font.h
@@ -3,14 +3,33 @@
 class Font
 {
 	public:
-		void Init( const std::string &fontName, int32_t fontSize )
+		Font()
+			:	mask( 0 )
+		{
+		}
+		bool Init( const std::string &fontName, int32_t fontSize )
 		{
 			if ( TTF_WasInit() == 0 )
-				TTF_Init();
+			{
+				if ( TTF_Init() == -1 )
+				{
+					std::cout << "Failed to init TTF! " << TTF_GetError() << std::endl;
+					return false;
+				}
 
+			}
 			font = TTF_OpenFont( fontName.c_str(), fontSize );
+
+			if ( font == nullptr )
+			{
+				std::cout << "Failed to load font";
+				return false;
+			}
 		}
+
 	
 	private:
 		TTF_Font* font;
+
+		int32_t mask;
 };
